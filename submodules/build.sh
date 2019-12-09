7b5a6a7a4f7bd006c00c45064db98d0d7b5a796a
Heaven <djyesu@gmail.com>
2019-12-09 11:18:28 +0800

    1

--- submodules/build.sh
+++ submodules/build.sh
@@ -4,7 +4,7 @@ NAMESPACE=sourcegarden
 NAME=openvpn-fss
 FULLNAME=registry.cn-hangzhou.aliyuncs.com/${NAMESPACE}/${NAME}
 #FULLNAME=$(NAMESPACE)/$(NAME)
-DOCKERFILE=./submodules/frpc/Dockerfile
+DOCKERFILE=./submodules/Dockerfile
 VERSION=$2
 
 
